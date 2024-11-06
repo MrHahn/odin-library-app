3be024c98c53e93663d099187d24de999d72e730
Christopher Hahn <chahn970@gmail.com>
2024-11-06 16:54:18 -0700

    create modal and form start on submit book function

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,10 @@
 const myLibrary = [];
 const libraryWrap = document.querySelector('.library-wrapper');
+const newBtn = document.querySelector('.new-book');
+const dialogBox = document.querySelector('dialog');
+const submitBtn = document.querySelector('form button');
+const closeBtn = document.querySelector('.close');
+const form = document.querySelector('form');
 
 
 function Book(title, author, pages, read) {
@@ -18,14 +23,22 @@ function displayLibrary() {
     libraryWrap.innerHTML = '';
     for (let book of myLibrary){
         let card =`
-        <div class="library-card">
-        <span class="title">${book.title}</span>
-        <span class="author">${book.author}</span>
-        <span class="pages">${book.pages}</span>
-        <span cass="read">${book.read}</span>
-        </div>
-
+            <div class="library-card">
+            <span class="title">${book.title}</span>
+            <span class="author">${book.author}</span>
+            <span class="pages">${book.pages}</span>
+            <span cass="read">${book.read}</span>
+            </div>
         `;
         libraryWrap.insertAdjacentHTML('beforeend', card);
     }
 }
+
+function submitNewBook(){
+    
+
+}
+
+newBtn.addEventListener('click', () => { dialogBox.showModal()});
+closeBtn.addEventListener('click', () => { dialogBox.close()});
+form.addEventListener('submit', submitNewBook );
