383e04ee672e8ebc3545f0968e4d27827feb247c
Christopher Hahn <chahn970@gmail.com>
2024-11-12 19:26:13 -0700

    complete functionality and layout

--- assets/scripts.js
+++ assets/scripts.js
@@ -34,8 +34,8 @@ function displayLibrary() {
         card.classList.add('library-card');
         card.innerHTML =`
             <span class="title">${book.title}</span>
-            <span class="author">${book.author}</span>
-            <span class="pages">${book.pages}</span>
+            <span class="author">Written by ${book.author}</span>
+            <span class="pages">${book.pages} pages</span>
         `;
         libraryWrap.appendChild(card);
         let removeBtn = addRemoveBtn(index);
@@ -74,7 +74,8 @@ function removeBook(index){
 
 function addRemoveBtn(index){
     let removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove Book';
+    removeBtn.classList.add('remove-book');
+    removeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>';
     removeBtn.setAttribute('data-index', index);
     removeBtn.addEventListener('click', () => {
         removeBook(index);
